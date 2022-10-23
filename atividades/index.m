b838f48c1b35be1d51f178b5859e301a547639e3
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-10-22 22:39:24 -0300

    index.m1

---
+++ atividades/index.m
@@ -0,0 +1,24 @@
+#Criar uma página web explicando o que é HTML, sendo obrigatório o uso de ao menos 10 tags.
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercicio01</title>
+    </head>
+    <body>
+        <h1>SEJAM BEM VINDOS!</h1>
+        <hr/>
+        <p>Vamos explicar o que significa <b>HTML!</b> </p>
+        
+        <p>Criada pelo britânico Tim Berners-Lee, o acrônimo HTML significa HiperText Markup Language, traduzindo ao português: Linguagem de Marcação de Hipertexto.  O HTML é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website. Portanto, ele serve para dar significado e organizar as informações de uma página na web. Sem isso, o navegador não saberia exibir textos como elementos ou carregar imagens e outros conteúdos.</p>
+        <br/>
+        <p> <I>Ao visitar uma página simples na web, você pode perceber que existem diferentes distribuições e tamanhos para títulos, parágrafos, imagens, vídeos e qualquer outro elemento. Essa estrutura é estabelecida através do HTML. No inicio da web, era comum encontrar sites apenas contendo textos e imagens simples, com estrutura básica e sem estilizações. Porém, nos dias atuais, muito dificilmente você encontrará sites que possuam apenas elementos HTML. Portanto, podemos considerar o HTML o “esqueleto” da sua página.</I></p>
+        <a href="https://www.homehost.com.br/blog/tutoriais/o-que-e-html/" target="_blank"> Clique <b>aqui</b> para acessar o contéudo completo!</a>
+        <BR/>
+        
+        <img src="download (1).jfif" alt="LOGOTIPOUMENTOR"
+        <br/>
+        <footer>UMENTOR OFICIAL</footer>
