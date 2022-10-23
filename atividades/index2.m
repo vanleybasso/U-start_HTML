f8695b223459d86bd8f8d3d4d1f61ef6bfdb3ed1
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-10-22 22:43:06 -0300

    index2.m

---
+++ atividades/index2.m
@@ -0,0 +1,30 @@
+# Criar uma página web com 3 imagens, cada imagem deve ter uma descrição. Sendo que uma dessas imagens precisa ter um link que direcione para algum site. Primeira Imagem deve ser de uma pasta local. Segunda Imagem deve vir de uma subpasta. Terceira Imagem deve vir de um link da web.
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>APRESENTAÇÃO DE IMAGENS U-START</title>
+
+    </head>
+    <body>
+        <h1>IMAGENS DE ALGUNS FAMOSOS NA ÁREA DE PROGRAMAÇÃO!</h1>
+        <hr/>
+        <p>TOP 3 DA HISTÓRIA!</p>
+        <br/>
+        <p><b>Alan Turing</b></p>
+        <img class = "alan_turing" src="C:\Users\aluno00\Documents\imagens_02/download.jfif" alt="Alan_Turing">
+        <br/>
+        <p><b>Ada Lovelace</b></p>
+        <img class = "ada" src="C:\Users\aluno00\Documents\imagens_02\ada_lovace/ada.jfif" alt="Ada">
+        <p><b>Bill Gates</b></p>
+        <img class= "bill" src="https://s2.glbimg.com/PUZaxeH10sfPu59n7vGYVEhii40=/620x430/e.glbimg.com/og/ed/f/original/2022/07/25/bill-gates.png" width="200" height="150" alt="bill_gates">
+        <br/>
+        <br/>
+
+
+        <a href="https://www.digitalhouse.com/br/blog/5-programadores-que-entraram-para-a-historia/" target="_blank"> Clique aqui para descobrir quais foram os principais programadores da história!</a>
+
+    </body>
