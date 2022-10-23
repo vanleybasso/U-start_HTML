46c1de6776c48904678d13e3a4da7adcb66da48b
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-10-22 22:44:34 -0300

    index3.m

---
+++ atividades/index3.m
@@ -0,0 +1,33 @@
+# Criar uma lista das linguagens de programação que você conhece. Seja criativo e ordene como achar melhor. Insira uma imagem da linguagem que você tem mais afinidade. Crie um campo de observações no final da sua lista.
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>LINGUAGENS DE PROGRAMAÇÃO</title>
+    </head>
+    <body>
+        <h1>REPERTÓRIO DE LINGUAGENS DE PROGRAMAÇÃO</h1>
+        <hr/>
+        <p>Observe abaixo a lista com meu histórico de estudo em programação: </p>
+            <ul>
+                <li>Python</li>
+                <img src="download.jfif" alt="Pyhton"
+                <br/>
+
+                <li>C++</li>
+                <img src="download.png" alt="C++"
+                <br/>
+                <li>Java</li>
+                <img src="download (1).jfif" alt="JAVA"
+                <br/>
+                <li>PHP</li>
+                <img src="download (1)CVVV.png" alt="PHP"
+                <br/>
+            </ul>
+        <hr/>
+        <p><i>TODOS ESSES CURSOS SÃO DE TOTAL CONHECIMENTO.</i></p>
+    </body>
+</html>
